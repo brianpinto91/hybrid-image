468f657d10407a78dd700fdf289b6e7706356dfb
brianpinto91 <brianpinto91@gmail.com>
2020-10-15 17:24:08 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hybridimg',
+      version = '0.1',
+      description = "A package which can be used to create hybrid image from two input images",
+      packages = ['hybridimg'],
+      author = ['Brian Pinto'],
+      license = 'The MIT License',
+      zip_safe = False)
